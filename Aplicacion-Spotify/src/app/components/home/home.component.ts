77d7bfec8f645659aed66b44a6ac0d045ae2f046
Daniel151505 <daniel150520011@gmail.com>
2021-07-05 23:59:47 -0500

    Pidiendo el arreglo

--- Aplicacion-Spotify/src/app/components/home/home.component.ts
+++ Aplicacion-Spotify/src/app/components/home/home.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+
+   }
 
   ngOnInit(): void {
   }
