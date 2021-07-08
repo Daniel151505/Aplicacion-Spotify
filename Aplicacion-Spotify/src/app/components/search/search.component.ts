53478a8723b6a4f53ff55a028ff01e1f11573ca3
Daniel151505 <daniel150520011@gmail.com>
2021-07-07 23:58:47 -0500

    Importando SpotifyService

--- Aplicacion-Spotify/src/app/components/search/search.component.ts
+++ Aplicacion-Spotify/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(spotify: SpotifyService) { }
 
   ngOnInit(): void {
   }
