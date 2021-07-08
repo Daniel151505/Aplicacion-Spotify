57576d7a0c12e3e0f8f704c6d5c5465751ec43e3
Daniel151505 <daniel150520011@gmail.com>
2021-07-07 22:13:15 -0500

    Llamando a servicio de Spotify

--- Aplicacion-Spotify/src/app/components/home/home.component.ts
+++ Aplicacion-Spotify/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  paises: any[] =[];
+  constructor(private spotify: SpotifyService) {
 
-  constructor(private http: HttpClient) {
-
-    this.http.get('https://restcountries.eu/rest/v2/lang/es')
-        .subscribe( (resp: any) => {
-          this.paises = resp;
-
-        })
-
+    this.spotify.getNewRealeases()
 
    }
 
