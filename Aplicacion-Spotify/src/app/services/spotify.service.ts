6127a781f87d1c0ac66543caa7effbe564a0e455
Daniel151505 <daniel150520011@gmail.com>
2021-07-07 22:50:22 -0500

    Return Service

--- Aplicacion-Spotify/src/app/services/spotify.service.ts
+++ Aplicacion-Spotify/src/app/services/spotify.service.ts
@@ -14,9 +14,7 @@ export class SpotifyService {
       'Authorization' : 'Bearer BQDoy_amKkRCu5zbX6pecR1RvZbzusspU-_75ryhlqoW66ZrA9o9_ZiBrlgzl4E2xjUI0USdIrvNlSOBcow'
     })
 
-    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
-    .subscribe(data => {
-      console.log(data);
-    })
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+
   }
 }
