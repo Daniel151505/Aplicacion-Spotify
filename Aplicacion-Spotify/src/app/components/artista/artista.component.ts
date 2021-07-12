84e0c5b7a25b113569f2a1d25824c9cc572a6fe9
Daniel151505 <daniel150520011@gmail.com>
2021-07-12 00:42:20 -0500

    Agregando Metodo getArtista

--- Aplicacion-Spotify/src/app/components/artista/artista.component.ts
+++ Aplicacion-Spotify/src/app/components/artista/artista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import { SpotifyService } from 'src/app/services/spotify.service';
 @Component({
   selector: 'app-artista',
   templateUrl: './artista.component.html',
@@ -7,13 +8,25 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ArtistaComponent implements OnInit {
 
-  constructor(private router: ActivatedRoute) {
-    this.router.params.subscribe(params =>{
+  artista: any = {}
 
+  constructor(private router: ActivatedRoute,
+              private spotify: SpotifyService) {
+    this.router.params.subscribe(params =>{
+      this.getArtista(params ['id'])
     })
    }
 
   ngOnInit(): void {
+  }
+
+  getArtista(id: string) {
+
+    this.spotify.getArtista(id)
+        .subscribe(artista =>{
+          this.artista = artista
+        })
+
   }
 
 }
